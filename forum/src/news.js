4b494473119f7ce4e1f22b0d8882eb4a9c7aac5f
Skuznetz <s.kuznetzov@mail.ru>
2018-06-15 22:25:56 +0400

    создадим компонент новостей

---
+++ forum/src/news.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class News extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Новости</h1>
+      </div>
+    );
+  }
+}
+
+export default News;
